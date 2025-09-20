b5b2d31f607578eab5b9341f03d13a7582a3be0b
Nethmi4 <cb015790@students.apiit.lk>
2025-09-20 23:08:08 +0530

    remove file

--- StuddyBuddy/pages/clear_cart.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/config.php';
-require_once __DIR__ . '/../config/helpers.php';
-require_login();
-check_csrf();
-
-unset($_SESSION['cart']);
-flash('ok', 'Cart cleared.');
-header('Location: cart.php');
